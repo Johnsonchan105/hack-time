f0e18a48fed33470248e8ee61481e30d3d7d7feb
RonKibel <52932245+RonKibel@users.noreply.github.com>
2019-10-19 16:08:40 -0700

    Update idontknwo.java

--- idontknwo.java
+++ idontknwo.java
@@ -12,8 +12,8 @@ import javafx.scene.control.Button;
 import javafx.scene.text.*;
 
 public class idontknwo extends Application {
-    ArrayList<String> totals = new ArrayList<String>();
-    int numOfGroups = 0;
+    private ArrayList<String> totals = new ArrayList<String>();
+    private int numOfGroups = 0;
     public static void main(String[] args) {
         launch(args);
     }
@@ -30,20 +30,27 @@ public class idontknwo extends Application {
         enter.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         pane.add(enter, 0, 0, 2, 1);
         pane.add(new Label("Name: "), 0, 2, 2, 1);
-        Button b = new Button("Next");
+        Button b = new Button("Store Name");
         pane.add(b, 0,3);
         Button b2 = new Button();
         b2.setText("Randomize");
         pane.add(b2, 0, 5, 2, 1);
         TextField tx = new TextField();
         pane.add(tx, 1, 2);
+        int p = 5;
+        int l = 0;
         //every time that next is pressed, total names increases
         b.setOnAction((ActionEvent e) -> {
             if (tx.getText() != null && !tx.getText().isEmpty())
                 totals.add(tx.getText());
+            if(p > 12){
+              l++;
+              p = 5;
+            }
+            pane.add(tx.getText(), l, p);
             tx.clear();
         });
-        pane.add(new Label("People per Group"), 0, 4, 2, 1);
+        pane.add(new Label("Number of Groups"), 0, 4, 2, 1);
         TextField tt = new TextField();
         pane.add(tt, 2, 4);
         tt.setOnAction(new EventHandler<ActionEvent>() {
@@ -64,11 +71,41 @@ public class idontknwo extends Application {
             //randomize group method
             primaryStage.close();
             GridPane pane2 = new GridPane();
+            pane2.setPadding(new Insets(50, 50, 50, 50));
+            pane2.setHgap(10);
+            pane2.setVgap(10);
+            //creates a new object
 
+            for(int j = 0; j < numOfGroups; j++){
+                String aaa = "Group " + j;
+                pane2.add(new Label(aaa), 0 , j);
+                for(int l = 1; l <  (totals.length() / numOfGroups); l++){
+                    pane2.add(new Label(randomize(totals)), j, l);
+                }
+            }
+
+
+
+            Scene scene2 = new Scene(pane2, 1000, 1000);
+            primaryStage.setTitle("Groups");
+            primaryStage.setScene(scene2);
+            primaryStage.show();
         });
 
 
     }
+    private String randomize(ArrayList<String> names){
+        for (int i = 0; i < 100; i++){
+          String a = names[(int)(Math.random()*names.length())];
+          String b = names[(int)(Math.random() * names.length())];
+          String temp = a;
+          a = b;
+          b = temp;
+        }
+        return(names[0]);
+        totals.remove(0);
+    }
+
     private void store(int a) {
       num = a;
     }
